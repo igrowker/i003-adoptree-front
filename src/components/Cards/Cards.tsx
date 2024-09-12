34131cda80716b0a7a7ff2c4e35a73bb392f5f09
moonstfw <nfacundoluna@gmail.com>
2024-09-12 19:23:20 -0300

    [style](landing): aumenta gap entre titulo-imagen

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -18,12 +18,21 @@ const Cards: React.FC = () => {
   ];
 
   return (
-    <div className="w-full flex flex-col items-center gap-5">
-      <span className='text-[#FF9900] font-facuFont'>Compra directa</span>
-      <h2 className="text-3xl font-manrope font-extrabold">Agricultura de otros productores</h2>
-      <div className='flex justify-center items-center gap-2'>
+    <div className="w-full flex flex-col items-center gap-10">
+      <div className="text-center">
+        <span className="text-[#FF9900] font-facuFont">Compra directa</span>
+        <h2 className="text-3xl font-manrope font-extrabold">
+          Agricultura de otros productores
+        </h2>
+      </div>
+      <div className="flex justify-center items-center gap-2">
         {cards.map((card, index) => (
-          <img className='w-[300px] h-[500px] ' key={index} src={card.thumbnail} alt={`Card ${index}`} />
+          <img
+            className="w-[300px] h-[500px] "
+            key={index}
+            src={card.thumbnail}
+            alt={`Card ${index}`}
+          />
         ))}
       </div>
     </div>
