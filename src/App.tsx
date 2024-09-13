414b14177c562d13db24d5e758c9404c3537ca78
marcelaTS79 <marcelina08ts@outlook.es>
2024-09-14 00:10:36 +0200

    Se agrego Banner

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Cards from './components/Cards/Cards';
 import Beneficios from './components/Beneficios/Beneficios';
+import Banner from './components/Banner/Banner';
 import './App.css';
 import Agriculture from './components/Agriculture/Agriculture';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Banner />
       <Cards />
       <Beneficios />
       <Agriculture />
