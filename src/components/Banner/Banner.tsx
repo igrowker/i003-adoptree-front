d6faa0f741bf5162d4d72c297ac7c9ac66ecfd0d
Merlina <noahchukita@gmail.com>
2024-09-17 01:13:00 -0300

    style(banner): ajustando estilos

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import './Banner.css'; 
+import './Banner.css';
 
 const Banner: React.FC = () => {
-    return (
-        <section className="banner">
-          <div className="banner-text">
-            <h1>
-              Adopta un árbol<br />
-              cítrico y sé parte<br />
-              de la agricultura<br />
-              sostenible.
-            </h1>
-          </div>
+  return (
+    <section className="banner">
+      <div className="banner-text">
+        <h1>
+          Adopta un árbol
+          <br />
+          cítrico y sé parte
+          <br />
+          de la agricultura
+          <br />
+          sostenible.
+        </h1>
+      </div>
     </section>
   );
 };
