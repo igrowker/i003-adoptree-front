c067bdebccda19032db94a1d8596efdd2042f26e
Merlina <noahchukita@gmail.com>
2024-09-18 12:19:03 -0300

    chore: formatea archivos

--- src/components/Agroadopcion/Agroadopcion.tsx
+++ src/components/Agroadopcion/Agroadopcion.tsx
@@ -20,8 +20,13 @@ const Agroadopcion: React.FC = () => {
                 Adoptá un cítrico de nuestro huerto de Entre Ríos. Vas a poder
                 verlo crecer a través de fotos en tu zona de usuario y recibir
                 tu cosecha de naranjas en tu casa. <br />
-                ¿Ya lo tenés decidido? Hacé <b><a href="#!" className="text-gray-700">clic acá</a></b> para comenzar la
-                adopción de tu naranjo. <br />
+                ¿Ya lo tenés decidido? Hacé{' '}
+                <b>
+                  <a href="#!" className="text-gray-700">
+                    clic acá
+                  </a>
+                </b>{' '}
+                para comenzar la adopción de tu naranjo. <br />
                 Gracias a las adopciones, podemos financiar las prácticas
                 regenerativas que estamos implementando para preservar los
                 ecosistemas locales.
@@ -37,7 +42,7 @@ const Agroadopcion: React.FC = () => {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-12 gap-4 mt-6">
-          <div className="md:col-span-3 mb-3 md:mb-0">
+            <div className="md:col-span-3 mb-3 md:mb-0">
               <img
                 src={imgNaranjero}
                 alt="imgNaranjero"
@@ -46,14 +51,30 @@ const Agroadopcion: React.FC = () => {
             </div>
             <div className="md:col-span-9 mb-9 md:mb-0">
               <p className="text-left text-gray-700 text-agro">
-              La producción actual de naranjas está reservada para los dueños de los árboles. De esta manera, podemos cultivar naranjas de forma ecológica bajo demanda y enviarlas directamente del árbol a tu casa, sin tratamientos postcosecha ni pasar por cámaras de frío.</p>
+                La producción actual de naranjas está reservada para los dueños
+                de los árboles. De esta manera, podemos cultivar naranjas de
+                forma ecológica bajo demanda y enviarlas directamente del árbol
+                a tu casa, sin tratamientos postcosecha ni pasar por cámaras de
+                frío.
+              </p>
 
-              <p className="text-left text-gray-700 text-agro mt-6 font-bold">¿Cómo podés llegar a ser dueño de un árbol?</p>
+              <p className="text-left text-gray-700 text-agro mt-6 font-bold">
+                ¿Cómo podés llegar a ser dueño de un árbol?
+              </p>
 
               <ol className="list-decimal px-6">
-                <li className="text-left">Decidí cuántas naranjas querés reservar para la primera temporada.</li>
+                <li className="text-left">
+                  Decidí cuántas naranjas querés reservar para la primera
+                  temporada.
+                </li>
                 <li className="text-left">Poné un nombre a tu árbol.</li>
-                <li className="text-left">Cuando confirmes la adopción de tu naranjo, te vamos a dar acceso a un huerto en tu área privada para que puedas seguir su evolución, planificar los envíos de tu cosecha, descargarte un certificado de propiedad o regalar el árbol a quien vos quieras.</li>
+                <li className="text-left">
+                  Cuando confirmes la adopción de tu naranjo, te vamos a dar
+                  acceso a un huerto en tu área privada para que puedas seguir
+                  su evolución, planificar los envíos de tu cosecha, descargarte
+                  un certificado de propiedad o regalar el árbol a quien vos
+                  quieras.
+                </li>
               </ol>
             </div>
           </div>
