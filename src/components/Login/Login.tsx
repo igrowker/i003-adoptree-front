024c5aeb4fc74d6ad7ebdad20766c5e8aaede9a1
Merlina <noahchukita@gmail.com>
2024-09-21 13:38:03 -0300

    style: ajustes en componente Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,60 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
 import backgroundImage from '../../assets/Hojas.png';
-import logo from '../../assets/AdopLogo.png';
-import facebookIcon from '../../assets/facebook.svg';
-import instagramIcon from '../../assets/instagram.svg';
-import twitterIcon from '../../assets/twitter.svg';
-import { Link } from 'react-router-dom'; 
-import './Login.css'; 
+import logo from '../../assets/Header.png';
+import google from '../../assets/google-svgrepo-com.svg'
+
+import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import './Login.css';
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { email, password, onInputChange } = useForm({
+    email: 'maria@mail.com',
+    password: '123456',
+  });
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
   };
 
   return (
-    <div className="login-container">
-      <div className="login-image">
+    <div className="login-container p-[50px]">
+      <div className="login-image shadow-sm">
         <img src={backgroundImage} alt="Background" />
       </div>
-      <div className="login-form">
-      <img src={logo} alt="Adoptree Logo" className="logo" />
-        <h2>Hola, Bienvenido</h2>
-        <p>Inicia sesión.</p>
+      <div className="login-form shadow-sm">
+        <img src={logo} alt="Adoptree Logo" className="logo w-[115px] mb-[30px]" />
+        <h2 className='mb-[30px]'>Hola, <br /> Bienvenido de nuevo</h2>
+        <p className='text-[14px]'>Inicie sesión para gestionar su cuenta.</p>
         <form onSubmit={handleLogin}>
-          <input 
-            type="email" 
-            placeholder="Email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required 
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={onInputChange}
+            required
           />
-          <input 
-            type="password" 
-            placeholder="Contraseña" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
+          <input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={onInputChange}
+            required
           />
 
-          <button type="submit" className="login-button">Login</button>
+          <button type="submit" className="bg-[#4BAF47] text-white border-none hover:bg-[#3B8838]">
+            Login
+          </button>
         </form>
 
-        <div className="social-login">
-          <p>Registro rápido</p>
-          <div className="social-media-icons">
-        <img src={facebookIcon} alt="Facebook" className="social-icon" />
-        <img src={instagramIcon} alt="Instagram" className="social-icon" />
-        <img src={twitterIcon} alt="Twitter" className="social-icon" />
-      </div>
+        <p className="line-text mt-[20px]">or</p>
+
+        <div className='flex items-center justify-center gap-3 mb-[40px]'>
+          <img className='w-[25px]' src={google} alt="google icon" />
+          <span>Iniciar sesión con Google</span>
         </div>
-        <p className="signup-text">
-        ¿Todavía no tenés cuenta? <Link to="/auth/registro">Registrate</Link>
+
+       
+        <p className="signup-text text-[14px] mt-[20px]">
+          ¿Todavía no tenés cuenta? <Link className='font-[500]' to="/auth/registro">Registrate</Link>
         </p>
       </div>
     </div>
