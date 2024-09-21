9872c66d0de99ece8fbcab57e111097ecff01809
moonstfw <nfacundoluna@gmail.com>
2024-09-21 11:45:07 -0300

    [feat](impacto-ambiental)Crear pagina de impacto ambiental

---
+++ src/pages/Impacto/Impacto.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ImpactoAmbiental from '../../components/ImpactoAmbiental/ImpactoAmbiental'
+
+const Impacto = () => {
+  return (
+    <ImpactoAmbiental /> 
+  )
+}
+
+export default Impacto
--- src/routes/AdoptreeRoutes.tsx
+++ src/routes/AdoptreeRoutes.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
 import Home from '../pages/Home/Home';
 import Checkout from '../pages/Checkout/Checkout';
+import Impacto from '../pages/Impacto/Impacto';
 
 const AdoptreeRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AdoptreeRoutes = () => {
         <Route path="/fincas" element="" />
         <Route path="/adopta-un-arbol" element="" />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/impacto-ambiental" element={<Impacto />} />
       </Routes>
 
       <Footer />
