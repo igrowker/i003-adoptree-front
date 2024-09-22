eaf31c3ea33c46af9575fe76a5283405a3b3b3e6
marcelaTS79 <marcelina08ts@outlook.es>
2024-09-22 15:07:03 +0200

    feat:Crea component DonacionesPatrocinos y Formularios para donaciones y patrocinios

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,8 +2,10 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import AdoptreeRoutes from '../routes/AdoptreeRoutes';
 import Login from '../components/Login/Login';
 import Registro from '../components/Login/Registro'
-import Contacto from '../components/Contacto/Contacto'; 
-
+import Contacto from '../components/Contacto/Contacto';
+import DonacionesPatrocinios from '../components/DonacionesPatrocinios/DonacionesPatrocinios';
+import DonacionForm from '../pages/DonacionesPatrocinios/DonacionForm';
+import PatrocinioForm from '../pages/DonacionesPatrocinios/PatrocinioForm';
 
 export const AppRouter = () => {
   // Antes de mostrar mis rutas, es decir, antes de hacer cualquier cosa en mi aplicación voy a hacer una evaluación
@@ -23,6 +25,9 @@ export const AppRouter = () => {
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/registro" element={<Registro />} />
         <Route path="/contacto" element={<Contacto />} />
+        <Route path="/donaciones-patrocinios" element={<DonacionesPatrocinios />} />
+        <Route path="/donaciones/formulario" element={<DonacionForm />} />
+        <Route path="/patrocinio/formulario" element={<PatrocinioForm />} />
       </>
       )}
       <Route path="/*" element={<Navigate to="/auth/login" replace/>} />
