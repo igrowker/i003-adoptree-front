d8911718e6a920092ceb5c8b7fba4b8a8c0816ab
Merlina <noahchukita@gmail.com>
2024-09-23 00:08:30 -0300

    feat: envuelve componente principal con el componente GoogleOAuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { AppRouter } from './router/AppRouter';
 import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App: React.FC = () => {
   return (
-    <>
+    <GoogleOAuthProvider clientId="320850219602-da35nosbb61qld5h0n2t254usmq207ia.apps.googleusercontent.com">
       <AppRouter />
-    </>
+    </GoogleOAuthProvider>
   );
 };
 
