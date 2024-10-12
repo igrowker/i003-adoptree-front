01782bb6074d11fdcd5f7ed9bbc255ec8efee864
AlexisPieropan <masterweb210@hotmail.com>
2024-10-12 19:57:54 -0300

    chore: correccion de tipos

--- src/pages/Adoptar/Adoptar.tsx
+++ src/pages/Adoptar/Adoptar.tsx
@@ -5,7 +5,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import { useGetArbolesQuery } from '../../store/services/arbolApi';
 import { useSearchParams } from 'react-router-dom';
 import './Adoptar.css';
+import { Arbol } from '../../store/services/arbolApi';
 
+// Eliminar 'ArbolD' y utilizar directamente 'Arbol' en el componente Adoptar
 const Adoptar: React.FC = () => {
   const { data: arboles, error, isLoading } = useGetArbolesQuery();
   const [searchParams] = useSearchParams();
@@ -18,20 +20,24 @@ const Adoptar: React.FC = () => {
     }
   }, [searchParams]);
 
- //Setear manualmente el nombre en plural para que el buscador lo reconozca sino no lo encuentra
- switch(searchTerm){
-  case "Naranjos" : setSearchTerm("Naranjo")
-                    break;
-  case "Pomeleros" : setSearchTerm("Pomelero")
-                    break;
-  case "Limoneros" : setSearchTerm("Limonero")
-                    break;
-  case "Mandarinos" : setSearchTerm("Mandarino")
-                    break;
- }
+  // Ajustar los términos de búsqueda según sea necesario
+  switch (searchTerm) {
+    case "Naranjos":
+      setSearchTerm("Naranjo");
+      break;
+    case "Pomeleros":
+      setSearchTerm("Pomelero");
+      break;
+    case "Limoneros":
+      setSearchTerm("Limonero");
+      break;
+    case "Mandarinos":
+      setSearchTerm("Mandarino");
+      break;
+  }
 
- //filtra y encuentra coincidencias en lowercase
-  const filteredArboles = arboles?.filter((arbol: any) =>
+  // Aplicar el filtro solo sobre las propiedades disponibles en el tipo 'Arbol'
+  const filteredArboles = arboles?.filter((arbol: Arbol) =>
     arbol.type.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -54,9 +60,7 @@ const Adoptar: React.FC = () => {
 
   return (
     <div className="px-4 sm:px-6 md:px-8 lg:px-[200px] 2xl:px-[165px] my-[116px]">
-      <h1 className="text-3xl sm:text-4xl font-bold mb-2 mt-6 sm:mt-8">
-        Adopciones
-      </h1>
+      <h1 className="text-3xl sm:text-4xl font-bold mb-2 mt-6 sm:mt-8">Adopciones</h1>
       <p className="text-gray-500 mb-4 sm:mb-6 max-w-2xl">
         Adopta un árbol para apoyar a un agricultor y recibe tu cosecha a lo largo de la temporada.
       </p>
@@ -65,8 +69,8 @@ const Adoptar: React.FC = () => {
         id="outlined-basic"
         label="Busca un árbol"
         variant="outlined"
-        value={searchTerm} // Setear el valor del campo
-        onChange={(e) => setSearchTerm(e.target.value)} // Cambiar el estado al escribir
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
@@ -75,9 +79,8 @@ const Adoptar: React.FC = () => {
           ),
         }}
       />
-
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 mt-10 max-w-screen-lg">
-        {filteredArboles?.map((arbol: any) => (
+        {filteredArboles?.map((arbol: Arbol) => (
           <AdoptarArbol key={arbol.id} datos={arbol} />
         ))}
       </div>
--- src/store/services/fincaApi.ts
+++ src/store/services/fincaApi.ts
@@ -1,13 +1,21 @@
 // src/services/fincaApi.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+export interface Finca {
+  id: number;
+  name: string;
+  ubication: string;
+  practicesSustainable: string;
+  productor: string;
+}
+
 const BACK_URL = import.meta.env.VITE_BACK_URL
 
 export const fincaApi = createApi({
   reducerPath: 'fincaApi',
   baseQuery: fetchBaseQuery({ baseUrl: `${BACK_URL}`  }),
   endpoints: (builder) => ({
-    getFincaData: builder.query({
+    getFincaData: builder.query<Finca[], void>({
       query: () => '/fincas',
     }),
   }),
