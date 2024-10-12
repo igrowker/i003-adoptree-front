138ff71372c0baa00827feba6a7464a49d5ba61d
AlexisPieropan <masterweb210@hotmail.com>
2024-10-12 20:03:42 -0300

    chore: correccion de tipos

--- src/store/services/fincaApi.ts
+++ src/store/services/fincaApi.ts
@@ -1,13 +1,6 @@
 // src/services/fincaApi.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export interface Finca {
-  id: number;
-  name: string;
-  ubication: string;
-  practicesSustainable: string;
-  productor: string;
-}
 
 const BACK_URL = import.meta.env.VITE_BACK_URL
 
@@ -15,7 +8,7 @@ export const fincaApi = createApi({
   reducerPath: 'fincaApi',
   baseQuery: fetchBaseQuery({ baseUrl: `${BACK_URL}`  }),
   endpoints: (builder) => ({
-    getFincaData: builder.query<Finca[], void>({
+    getFincaData: builder.query({
       query: () => '/fincas',
     }),
   }),
